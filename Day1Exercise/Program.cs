dc126a4348588d30cca5b5c40dad2c3c459fe913
aronunlabeled <xaron97x@hotmail.com>
2021-09-27 15:31:10 -0400

    second

--- Day1Exercise/Program.cs
+++ Day1Exercise/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
 namespace Day1Exercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String again;
+            double length;
+            double width;
+            double perimeter;
+            double area;
+            double carpet;
+            double cans;
+            do{
+                Console.WriteLine("Enter Length");
+                length = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Width");
+                width = double.Parse(Console.ReadLine());
+                perimeter = (2 * length) + (2 * width);
+                area = length * width;
+                carpet = area / 5;
+                cans = perimeter / 5;
+                Console.WriteLine("The area is " + area + ".");
+                Console.WriteLine("The perimeter is " + perimeter + ".");
+                Console.WriteLine(cans + " cans of paint will be needed.");
+                Console.WriteLine(carpet + " tiles of carpet will be needed");
+                Console.WriteLine("Calculate another room?: y or n");
+                again = Console.ReadLine();
+            } while (again == "y" || again == "Y");
         }
     }
 }
